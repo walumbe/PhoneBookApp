77479183a9a89add4335d077a468731cc5473e90
walumbe <nathanwalumbe@gmail.com>
2023-01-30 17:32:31 +0300

    implemented search by lastname functionality

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Contact extends Model
 {
     use HasFactory;
+    protected $fillable = ['firstname', 'lastname', 'phone', 'email'];
+    public function scopeFilter($query, array $filters) {
+        if($filters['lastname'] ?? false) {
+            $query->where('lastname', 'like', '%' . request('lastname'). '%');
+        }
+
+        
+        if($filters['search'] ?? false) {
+            $query->where('lastname', 'like', '%' . request('search') . '%');
+        }
+    }
 }
